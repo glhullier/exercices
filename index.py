8106ca8f6cb7cb5d9e80dd1d99eca12a4d3116f8
glhullier <g.lhullier@it-students.fr>
2024-07-30 11:43:26 +0200

    surface

--- index.py
+++ index.py
@@ -1,4 +1,7 @@
-def hello():
-    return "hello edabit.com"
+def tri_area(base,height):
+    return base*height/2
 
-print(hello())
+print(tri_area(3,2))
+print(tri_area(7,4))
+print(tri_area(10,10))
+
