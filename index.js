17534ba5fe80d1e8d84a97e612ba7592fd25156e
glhullier <g.lhullier@it-students.fr>
2024-08-04 14:20:44 +0200

    array

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-function cube(number) {
-  return number * number * number;
+function getFirstElement(number) {
+  return number[0];
 }
 
-console.log(cube(3));
-console.log(cube(5));
-console.log(cube(10));
+console.log(getFirstElement([1, 2, 3]));
+console.log(getFirstElement([80, 5, 100]));
+console.log(getFirstElement([-500, 0, 50]));
