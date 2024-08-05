175d7d08fc0f57a7de8f18cf84fe54e6e3af37f5
glhullier <g.lhullier@it-students.fr>
2024-08-05 13:22:32 +0200

    delete

--- index.js
+++
@@ -1,7 +0,0 @@
-function getFirstElement(number) {
-  return number[0];
-}
-
-console.log(getFirstElement([1, 2, 3]));
-console.log(getFirstElement([80, 5, 100]));
-console.log(getFirstElement([-500, 0, 50]));
