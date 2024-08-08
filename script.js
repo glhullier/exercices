fc1a01889b84e63b5d77f2b45c93c9106ae22311
glhullier <g.lhullier@it-students.fr>
2024-08-08 13:34:38 +0200

    script

---
+++ script.js
@@ -0,0 +1,9 @@
+function nextEdge(side1, side2) {
+  if (side1 > 0 && side2 > 0) {
+    return side1 + side2 - 1;
+  }
+}
+
+console.log(nextEdge(8, 10));
+console.log(nextEdge(5, 7));
+console.log(nextEdge(9, 2));
