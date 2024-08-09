a3040d460ec99f309cf2034d31fa984c0b01abf2
glhullier <g.lhullier@it-students.fr>
2024-08-09 12:49:55 +0200

    edge

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function nextEdge(side1, side2) {
-  if (side1 > 0 && side2 > 0) {
-    return side1 + side2 - 1;
+  if (side1 && side2) {
+    return side1 % side2;
   }
 }
 
-console.log(nextEdge(8, 10));
-console.log(nextEdge(5, 7));
-console.log(nextEdge(9, 2));
+console.log(nextEdge(1, 3));
+console.log(nextEdge(3, 4));
+console.log(nextEdge(-9, 45));
+console.log(nextEdge(5, 5));
