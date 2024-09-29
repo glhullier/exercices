69620046181c0d7930539545e8b1f67fd788cae7
glhullier <g.lhullier@it-students.fr>
2024-09-29 16:29:43 +0200

    sass

--- index.php
+++ index.php
@@ -10,10 +10,31 @@
 <body>
     <h1>Il fait beau aujourd'hui</h1>
 
-    <?php    
+    <?php 
+    $condition = 0;
+    $boucle = 0;   
     $tableau = array (56,45,76);//tableau
-    echo $tableau[1]"<br/>";
+    echo $tableau[1]."<br/>";
     $data = 20;
+    $prenoms= array('Pierre', 'Paul', 'Jacques');//tableau
+    echo 'Le second prénom du tableau est:'.$prenoms[1].'<br/>';
+    $age = array('Pierre'=>1, 'Paul'=>2, 'Jacques'=>3);//Tableau associatif
+    echo 'L\'age de Paul est égal à :'.$age['Paul'].'<br/>';
+    while ( $boucle<=3){
+        echo $boucle."<br/>";//boucle while
+        $boucle++;
+    }
+    do{
+        echo 'Ceci est le résultat de la boucle DOWHILE:'.$condition.' <br/>';
+        $condition++;
+    } while($condition < 4);//boucle dowhile
+
+   for($bcle=0; $bcle<4;$bcle++){
+    echo 'Ceci est le résultat de la boucle for:'.$condition.'<br/>';
+    $bcle++;
+
+   }
+
     $donnee = ($data<30)? "data est inférieur à 30 <br/>" : "data est supérieur à 30 <br/>";//ternaire
     echo $donnee;
     $variable = "Ceci est une variable";
--- script.js
+++ script.js
@@ -1,1 +1,3 @@
 alert("Je vous dis bonjour");
+
+console.log("J'affiche mon console log");
