16707cc348536873e7f392e854b5cbdecf8543ef
glhullier <g.lhullier@it-students.fr>
2024-10-16 14:09:05 +0200

    date

--- page.php
+++ page.php
@@ -19,6 +19,47 @@ echo strpos('Bonjour à tous', 'tous').'Ce chiffre s\'obtient grâce à la fonct
 $str = 'J\'aime le<strong> "PHP"</strong>';
 echo htmlspecialchars($str)."La fonction htmlspecialchars garde le html<br/>";
 
+$voitures = array(
+    "Citroen"=>"DS3",
+    "Renault"=>"Clio",
+);
+
+echo "<pre>";
+print_r(array_values($voitures));
+echo "</pre>";
+echo "Ce tableau s'affiche lisiblement";
+echo "<br/><br/>";
+
+if(array_key_exists("Citroen",$voitures)){
+    echo "La clé existe";
+}else{
+    echo "La clé n'existe pas";
+}
+echo "  Cle trouvee grace à la méthode array_key_exists<br/><br/>";
+
+$compare= array( "a"=>"bleu", "b"=>"noir","c"=>"brun" );
+$comparant = array("a"=>"bleu", "b"=>"noir" );
+
+print_r(array_diff_assoc($compare,$comparant));
+echo"Ceci est le resultat de la methode array_diff_assoc <br/><br/>";
+
+
+print_r(array_keys($voitures));
+echo "<br/>La méthode array_keys affiche la clé de chaque élément du tableau voitures <br/>";
+
+$phrase = array ("Bonjour", "à","tous");
+echo implode(" ",$phrase)."Phrase obtenue à partir d'un tableau grâce à la méthode implode <br/>";
+echo "<br/><br/>";
+$mot = "Bonjour à tous";
+print_r (explode(" ",$mot));
+echo "<br/>Transformation d'un string en array grâce à la méthode explode<br/>";
+echo "<br/><br/>";
+
+echo date("d-m-Y")."<br/><br/>";
+echo "Date affichee grace à la méthode date";
+
+echo "<br/><br/>";
+
 
 
 ?>
